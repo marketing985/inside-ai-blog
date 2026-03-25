c93145dbb82bda59fe17050f56f6e5e46c5c35bf
hr738 <hr@houseofaetheria.com>
2026-03-25 21:36:39 +0530

    feat: build homepage magazine grid layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://inside.ai',
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), mdx()],
 });
