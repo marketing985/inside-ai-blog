292727156fa5a003924c6e1d466be2299acbb135
hr738 <hr@houseofaetheria.com>
2026-03-27 21:20:56 +0530

    fix: remove sitemap plugin (version incompatibility)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,9 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://insideai.in',
-  integrations: [react(), tailwind(), mdx(), sitemap()],
+  integrations: [react(), tailwind(), mdx()],
 });
